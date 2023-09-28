825e32eec57cd66c55ef8db65c9a22ffa4803e0c
Vivek Rathod <107935618+vivekrathod260@users.noreply.github.com>
2023-09-28 11:26:28 +0530

    Time: 13 ms (17.42%), Space: 16.5 MB (36.74%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -20,6 +20,15 @@ public:
             swap(nums[i], nums[j]);
         }
 
+        // while (i < j) {
+        //     while (i < j && nums[i] % 2 == 0)
+        //         i++;
+        //     while (i < j && nums[j] % 2 == 1)
+        //         j--;
+            
+        //     std::swap(nums[i], nums[j]);
+        // }
+
         return nums;
     }
 };
