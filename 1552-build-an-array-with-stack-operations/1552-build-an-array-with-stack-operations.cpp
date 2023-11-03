acab197eb435fa3f44b3d071cadda4f0bab90231
Vivek Rathod <107935618+vivekrathod260@users.noreply.github.com>
2023-11-03 10:00:47 +0530

    Time: 4 ms (39%), Space: 8.1 MB (75.36%) - LeetHub

--- 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.cpp
+++ 1552-build-an-array-with-stack-operations/1552-build-an-array-with-stack-operations.cpp
@@ -3,23 +3,10 @@ public:
     vector<string> buildArray(vector<int> &t, int N) {
         vector<string> ans;
         int n = t.size();
-        int top = INT_MIN;
+        int top = 0;    //think   for oth iteration
         
         for(int i = 0; i<n; i++)
         {
-            if(i==0)
-            {
-                for(int j = 0; j<(t[i]-1); j++)     // think test case [5,6,7]
-                {
-                    ans.push_back("Push");
-                    ans.push_back("Pop");
-                }
-
-                ans.push_back("Push");
-                top = t[i];
-                continue;
-            }
-
             for(int j = 0; j<(t[i]-top-1); j++)
             {
                 ans.push_back("Push");
