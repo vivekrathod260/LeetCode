6d4f5f995049f7cf8758851be8569691fefcb1bd
Vivek Rathod <107935618+vivekrathod260@users.noreply.github.com>
2023-11-06 13:47:54 +0530

    Time: 477 ms (13.63%), Space: 190.8 MB (18.79%) - LeetHub

--- 1955-seat-reservation-manager/1955-seat-reservation-manager.cpp
+++ 1955-seat-reservation-manager/1955-seat-reservation-manager.cpp
@@ -1,42 +1,65 @@
 class SeatManager {
-    vector<int> v;
     int n;
-    int cur;
     set<int> s;
 public:
     SeatManager(int n) {
-        vector<int> vec(n,0);
-        v = vec;
         this->n = n;
-        cur = 0;
+        for(int i = 1; i<=n; i++) s.insert(i);
     }
     
     int reserve() {
-        v[cur] = 1;
-        int ans = cur+1;
-
-        if(s.size()!=0)
-        {
-            cur = *s.begin(); s.erase(s.begin());
-        }
-        else
-        {
-            cur++;
-        }
-
+        int ans = *s.begin(); s.erase(s.begin());
         return ans;
     }
     
     void unreserve(int seatNumber) {
-        v[seatNumber-1] = 0;
-
-        // consider reserved till 9 unreserve 4, unreserve 6
-        s.insert(cur);
-        s.insert(seatNumber-1);
-        cur = *s.begin(); s.erase(s.begin());
+        s.insert(seatNumber);
     }
 
 };
+
+
+// approach 2
+
+// class SeatManager {
+//     vector<int> v;
+//     int n;
+//     int cur;
+//     set<int> s;
+// public:
+//     SeatManager(int n) {
+//         vector<int> vec(n,0);
+//         v = vec;
+//         this->n = n;
+//         cur = 0;
+//     }
+    
+//     int reserve() {
+//         v[cur] = 1;
+//         int ans = cur+1;
+
+//         if(s.size()!=0)
+//         {
+//             cur = *s.begin(); s.erase(s.begin());
+//         }
+//         else
+//         {
+//             cur++;
+//         }
+
+//         return ans;
+//     }
+    
+//     void unreserve(int seatNumber) {
+//         v[seatNumber-1] = 0;
+
+//         // consider reserved till 9 unreserve 4, unreserve 6
+//         s.insert(cur);
+//         s.insert(seatNumber-1);
+//         cur = *s.begin(); s.erase(s.begin());
+//     }
+
+// };
 
 /**
  * Your SeatManager object will be instantiated and called as such:
